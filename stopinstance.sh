026b0861866f89ae5da238dab43b619b6e65d365
sborge1413 <sborge1413@gmail.com>
2022-04-19 06:36:22 +0530

    my update

--- stopinstance.sh
+++ stopinstance.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 stopcmd=stop-instances
 inid=--instance-ids 
-id="i-01f0b984c59e1883d"
+Instance_id="i-0638ee70ccaedc6fa"
 
 
 #aws ec2 stop-instances --instance-ids <id>
-aws ec2 $stopcmd $inid $id
+aws ec2 $stopcmd $inid $Instance_id
 
 
